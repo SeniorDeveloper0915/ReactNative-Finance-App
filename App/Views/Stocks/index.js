de2b4c1213eabafb90c5f9a4197beeaba5e76aeb
SeniorDeveloper0915 <senior.web.developer.0915@outlook.com>
2019-05-21 05:45:12 -0700

    Merge branch 'develop'

--- App/Views/Stocks/index.js
+++ App/Views/Stocks/index.js
@@ -24,9 +24,6 @@ var StockStore = require('../../Utils/Stock/store');
 // View Elements
 var StockCell = require('./Elements/StockCell');
 
-// Views
-var WebView = require('../Web');
-
 // Styles
 var styles = require('./style');
 
